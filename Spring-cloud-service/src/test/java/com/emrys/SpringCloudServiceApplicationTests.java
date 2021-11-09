2d3fda71b7b18f31aa521188ea6617ae4400d20f
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-11-09 17:50:43 -0500

    spring-cloud-service

---
+++ Spring-cloud-service/src/test/java/com/emrys/SpringCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
